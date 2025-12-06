1f79ea9941f154501839a5613cccdd87f8f4c1ab
JuanMatosTezanos <juan.matos1@snhu.edu>
2025-12-06 17:34:38 -0600

    Made changes to trip-listing.ts to implement "OnIt" and made changes to trips_listing.html

--- app_admin/src/app/trip-listing/trip-listing.ts
+++ app_admin/src/app/trip-listing/trip-listing.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { trips } from '../data/trips';
 
 @Component({
   selector: 'app-trip-listing',
@@ -6,6 +8,13 @@ import { Component } from '@angular/core';
   templateUrl: './trip-listing.html',
   styleUrl: './trip-listing.css',
 })
-export class TripListing {
+export class TripListing implements OnInit{
+  trips: Array<any> = trips;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    
+  }
 
 }
