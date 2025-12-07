7e794796c5663a39015178cc19aed347eab39cd3
JuanMatosTezanos <juan.matos1@snhu.edu>
2025-12-06 20:48:43 -0600

    fixed error where images would disappear due to a faulty array.  Created add-trip.

--- app_admin/src/app/trip-listing/trip-listing.ts
+++ app_admin/src/app/trip-listing/trip-listing.ts
@@ -1,21 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule,JsonPipe,CurrencyPipe } from '@angular/common';
-import { trips } from '../data/trips';
+import { CommonModule, JsonPipe, CurrencyPipe } from '@angular/common';
+import { trips } from '../data/trips';   // <-- static array import
 import { TripCard } from '../trip-card/trip-card';
 
 @Component({
   selector: 'app-trip-listing',
-  imports: [JsonPipe,CurrencyPipe,CommonModule,TripCard],
+  standalone: true,
+  imports: [JsonPipe, CurrencyPipe, CommonModule, TripCard],
   templateUrl: './trip-listing.html',
   styleUrl: './trip-listing.css',
 })
-export class TripListing implements OnInit{
-  trips: Array<any> = trips;
+export class TripListing implements OnInit {
+  trips: Array<any> = trips;   // <-- static array assignment
+  message: string = '';
 
-  constructor() {}
-
-  ngOnInit(): void {
-    
+  constructor() {
+    console.log('trip-listing constructor');
+    this.message = this.trips.length > 0
+      ? 'There are ' + this.trips.length + ' trips available.'
+      : 'There were no trips retrieved from the database';
+    console.log(this.message);
   }
 
+  ngOnInit(): void {
+    console.log('ngOnInit');
+  }
 }
+
+
+
+
