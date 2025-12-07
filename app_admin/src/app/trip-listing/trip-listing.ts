4c88b8faa790682e503f482ee296a11bf87d6f84
JuanMatosTezanos <juan.matos1@snhu.edu>
2025-12-06 19:44:56 -0600

    Enable Cors and fixed error where images would not show on the website

--- app_admin/src/app/trip-listing/trip-listing.ts
+++ app_admin/src/app/trip-listing/trip-listing.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule,JsonPipe,CurrencyPipe } from '@angular/common';
 import { trips } from '../data/trips';
+import { TripCard } from '../trip-card/trip-card';
 
 @Component({
   selector: 'app-trip-listing',
-  imports: [JsonPipe,CurrencyPipe,CommonModule],
+  imports: [JsonPipe,CurrencyPipe,CommonModule,TripCard],
   templateUrl: './trip-listing.html',
   styleUrl: './trip-listing.css',
 })
