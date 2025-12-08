f325475f9123320f2de29b11aadd1e04219a6d29
JuanMatosTezanos <juan.matos1@snhu.edu>
2025-12-07 21:02:37 -0600

    Updated trip-listing.ts

--- app_admin/src/app/app.ts
+++ app_admin/src/app/app.ts
@@ -1,10 +1,11 @@
 import { CommonModule} from '@angular/common';
 import { Component, signal } from '@angular/core';
-import { TripListing } from './trip-listing/trip-listing';
+//import { TripListing } from './trip-listing/trip-listing';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [ CommonModule, TripListing],
+  imports: [ CommonModule, RouterModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- app_admin/src/app/trip-listing/trip-listing.ts
+++ app_admin/src/app/trip-listing/trip-listing.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { trips } from '../data/trips';   // <-- static array import
 import { TripCard } from '../trip-card/trip-card';
+import { Trip } from '../models/trip';
+import { Router } from '@angular/router';
+import { TripData } from '../services/trip-data';
 
 @Component({
   selector: 'app-trip-listing',
@@ -9,24 +12,30 @@ import { TripCard } from '../trip-card/trip-card';
   imports: [CommonModule, TripCard],
   templateUrl: './trip-listing.html',
   styleUrl: './trip-listing.css',
+  providers: [TripData]
 })
 export class TripListing implements OnInit {
   trips: Array<any> = trips;   // <-- static array assignment
   message: string = '';
 
-  constructor() {
-    console.log('trip-listing constructor');
-    this.message = this.trips.length > 0
-      ? 'There are ' + this.trips.length + ' trips available.'
-      : 'There were no trips retrieved from the database';
-    console.log(this.message);
-  }
+  constructor(private router: Router, private tripDataService: TripData) {
+  console.log('trip-listing constructor');
+  this.message = this.trips.length > 0
+    ? 'There are ' + this.trips.length + ' trips available.'
+    : 'There were no trips retrieved from the database';
+  console.log(this.message);
+}
 
   ngOnInit(): void {
     console.log('ngOnInit');
   }
-}
 
+ public addTrip(): void {
+    this.router.navigate(['add-trip']);
+  
+  }
+
+}
 
 
 
